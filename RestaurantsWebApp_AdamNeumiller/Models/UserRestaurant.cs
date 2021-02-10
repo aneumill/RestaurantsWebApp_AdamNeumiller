8029cae5d79c7124dc532db63247a5e8a8502707
aneumill <71047690+aneumill@users.noreply.github.com>
2021-02-10 11:06:09 -0700

    Some updates

--- RestaurantsWebApp_AdamNeumiller/Models/UserRestaurant.cs
+++ RestaurantsWebApp_AdamNeumiller/Models/UserRestaurant.cs
@@ -14,7 +14,7 @@ namespace RestaurantsWebApp_AdamNeumiller.Models
         public string RestaurantName { get; set; }
         [Required]
         public string Dish { get; set; }
-        [Phone, Required, RegularExpression("[0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]")]
+        [Phone, Required, RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Phone number must be 10 digits. Ex. 111-111-1111 or 111.111.1111 or 1111111111")]
         public string RestaurantPhone { get; set; }
     }
 }
