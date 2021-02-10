a6bcaa7331150071005b378ee437c556911b7bdd
aneumill <71047690+aneumill@users.noreply.github.com>
2021-02-10 15:26:25 -0700

    Some more changes

--- RestaurantsWebApp_AdamNeumiller/Models/Restaurants.cs
+++ RestaurantsWebApp_AdamNeumiller/Models/Restaurants.cs
@@ -19,10 +19,10 @@ namespace RestaurantsWebApp_AdamNeumiller.Models
         public string sRestaurantName { get; set;  }
         
         public string sRestaurantDish { get; set; } = "It's all tasty!";
- 
-        public string sRestaurantAddress { get; set;  }
         [Required]
-        public string sRestaurantPhone { get; set; }
+        public string sRestaurantAddress { get; set;  }
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$")]
+        public string? sRestaurantPhone { get; set; } = "";
         public string? sRestaurantWebsite { get; set; } = "Coming Soon!";
         public static Restaurant[] GetRestaurants() 
         {
@@ -31,7 +31,7 @@ namespace RestaurantsWebApp_AdamNeumiller.Models
                 sRestaurantName = "Don Joaquins",
                 sRestaurantAddress = "150 W 1230 N St, Provo, UT 84604",
                 sRestaurantDish = "Pollo Burritto",
-                sRestaurantPhone = "(801) 400-2894",
+                sRestaurantPhone = "801-400-2894",
                 sRestaurantWebsite = "https://stdonjoaquin.com/"
             };
             Restaurant r2 = new Restaurant(2)
@@ -40,7 +40,7 @@ namespace RestaurantsWebApp_AdamNeumiller.Models
                 sRestaurantName = "J-Dawgs",
                 sRestaurantAddress = "858 700 E, Provo, UT 84606",
                 sRestaurantDish = "Polish Dawg",
-                sRestaurantPhone = "(801) 842-1898",
+                sRestaurantPhone = "801-842-1898",
                 sRestaurantWebsite = "https://jdawgs.com/",
             };
             Restaurant r3 = new Restaurant(3)
@@ -49,7 +49,7 @@ namespace RestaurantsWebApp_AdamNeumiller.Models
                 sRestaurantName = "Bombay House",
                 sRestaurantAddress = "123 Sesame Street",
                 sRestaurantDish = "Chicken Vindaloo",
-                sRestaurantPhone = "(801) 373-6677",
+                sRestaurantPhone = "801-373-6677",
                 sRestaurantWebsite = "https://bombayhouse.com/",
             };
             Restaurant r4 = new Restaurant(4)
@@ -58,7 +58,7 @@ namespace RestaurantsWebApp_AdamNeumiller.Models
                 sRestaurantName = "Asa Ramen",
                 sRestaurantAddress = "1120 State St, Orem, UT 84097",
                 sRestaurantDish = null,
-                sRestaurantPhone = "(801) 842-1898",
+                sRestaurantPhone = "801-842-1898",
                 sRestaurantWebsite = null,
             }; 
             Restaurant r5 = new Restaurant(5)
@@ -67,7 +67,7 @@ namespace RestaurantsWebApp_AdamNeumiller.Models
                 sRestaurantName = "Brick Oven",
                 sRestaurantAddress = "111 E 800 N, Provo, UT 84606",
                 sRestaurantDish = "Margarita Pizza",
-                sRestaurantPhone = "(801) 374-8800",
+                sRestaurantPhone = "801-374-8800",
                 sRestaurantWebsite = "https://www.brickovenrestaurants.com/",
             };
             return new Restaurant[] { r1, r2, r3, r4, r5 }; 
